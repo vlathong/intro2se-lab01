bc098360060149318aa03d725d1da563d729d710
vlathong <105715507+vlathong@users.noreply.github.com>
2022-06-24 15:54:49 +0700

    123
    123

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Navbar from './components/Navbar';
 import{BrowserRouter as Router,Route,Routes} from "react-router-dom"
 import Home from './pages/Home';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
@@ -10,8 +12,9 @@ function App() {
       <Router>
         <Navbar />
           <Routes>
-            <Route path="/"exact component={Home}/>
+            <Route exact path='/' element={<Home />} />
           </Routes>
+          <Footer/>
       </Router>
     </div>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+import SvgIcon from '@mui/material/SvgIcon';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+          <div className="socialMedia">
+            
+          </div>
+          <p> &copy; 2022 nhacnhushit.com</p>
+        </div>
+      );
+}
+
+export default Footer
